eeb7d43a95fab715c58f2cafa735ea6bfbebdd09
Akshay Kumar S B <jinmori58690@gmail.com>
2023-01-29 18:14:06 +0530

    Update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -13,13 +13,13 @@ LOG.setLevel(level=logging.INFO)
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',}
 
-API_ID = int(environ.get('API_ID', 45647)) #API ID
-API_HASH = environ.get('API_HASH', 'Your Api Id') #API HASH
-BOT_TOKEN = environ.get('BOT_TOKEN', 'Your Bot Token') #BOT TOKEN
-DATABASE_URL = environ.get('DATABASE_URL', 'Your MongoDb') #MONGO DB
-OWNER_ID = int(environ.get('OWNER_ID', None)) #OWNER ID
-MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', None))#YOUR MAIN CHANNEL ID
-ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', None))#YOUR ARCHIVE CHANNEL
+API_ID = int(environ.get('API_ID', 24833791)) #API ID
+API_HASH = environ.get('API_HASH', "42488cb247a33d13d5f97d6839c8e52b") #API HASH
+BOT_TOKEN = environ.get('BOT_TOKEN', "5898463551:AAEOz_2P6j9xrsrDzvs_Hgz9GERU4N8kqog") #BOT TOKEN
+DATABASE_URL = environ.get('DATABASE_URL', "") #MONGO DB
+OWNER_ID = int(environ.get('OWNER_ID', "2064735436")) #OWNER ID
+MAIN_CHANNEL = int(environ.get('MAIN_CHANNEL', "-1001886875139"))#YOUR MAIN CHANNEL ID
+ARCHIVE_CHANNEL = int(environ.get('ARCHIVE_CHANNEL', "-1001249680167"))#YOUR ARCHIVE CHANNEL
 MESSAGE_ID = int(environ.get('MESSAGE_ID', None)) #SUB CHANNEL STATUS ID
 
 soheru = Client('SoheruBots', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="Bot/plugins"))
